a9d5e92d38bad24da55a68bb7f241e9e25456bf7
HaoXu <hxxu4@student.unimelb.edu.au>
2022-09-06 20:21:37 +1000

    Added date into dashboard

--- start_page/src/dashBoard/Information.js
+++ start_page/src/dashBoard/Information.js
@@ -1,19 +1,33 @@
 import React from 'react'
 import {FaSearch} from 'react-icons/fa'
+import { useState } from 'react'
 
 const Information = ({search},{setSearch}) => {
+
+  const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+  ];
+  
+  const [date, setDate]=useState(new Date());
+
  
  
   
 
   return (
-    <div className='Information'>
-        Main information area
-        <form className='SearchForm' onSubmit={(e)=>e.preventDefault}>
-            <label htmlFor='search'><FaSearch className='FaSearch' size={25}/></label>
-            <input id='search' type='text' placeholder='Search Records' value={search} onChange={(e)=>setSearch(e.target.value)} ></input>
-        </form>
-    </div>
+    <>
+      
+      <div className='Information'>
+        <h2 className='TimeDisplay'>
+          {date.getFullYear()} <br/> {monthNames[date.getMonth()]}
+        </h2>        
+      </div>
+      <form className='SearchForm' onSubmit={(e)=>e.preventDefault}>
+        <label htmlFor='search'><FaSearch className='FaSearch' size={25}/></label>
+        <input id='search' type='text' placeholder='Search Records' value={search} onChange={(e)=>setSearch(e.target.value)} ></input>
+      </form>
+
+    </>
   )
 }
 
