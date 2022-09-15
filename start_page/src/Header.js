1f80e7b8c214fdf6f5ed68d7a0fa1fdee6007f65
HaoXu <87160664+AugustRushG@users.noreply.github.com>
2022-09-15 12:00:44 +1000

    Log Out function completed

--- start_page/src/Header.js
+++ start_page/src/Header.js
@@ -1,11 +1,22 @@
 import React from 'react'
-import{Link,useLocation} from 'react-router-dom'; 
+import{Link,useNavigate,useLocation} from 'react-router-dom'; 
 import useAuth from './hooks/useAuth';
+import useLogOut from './hooks/useLogOut';
 
 const Header = () => {
     //get current location
     const location=useLocation();
     console.log("current is "+location.pathname);
+
+    const navigate=useNavigate();
+    const logOut=useLogOut();
+
+
+    //function to help user log out
+    const signOut=async()=>{
+      await logOut();
+      navigate('/')
+    }
 
  
 
@@ -17,7 +28,7 @@ const Header = () => {
         <h1><Link to='/'>RecordIt</Link></h1>
         {location.pathname.includes('/dashboard')? (<>
             <Link to = '/LogIn'><button type = "button" className='Account'>Account</button></Link>
-            <Link to = '/'> <button type = "button" className='LogOut'>LogOut</button></Link>
+            <button type = "button" className='LogOut' onClick={signOut}>LogOut</button>
         </>):(<>
             <Link to = '/LogIn'> <button type = "button" className='LogIn'>LogIn</button></Link>
             <Link to = '/Register'> <button type = "button" className='SignUp'>SignUp</button></Link>
---
+++ start_page/src/hooks/useLogOut.js
@@ -0,0 +1,24 @@
+import axios from "../api/axios";
+import useAuth from "./useAuth";
+
+const LOGOUT_URL='api/users/logout';
+const useLogOut=()=>{
+    const {setAuth}=useAuth();
+    const logOut=async()=>{
+        //set auth to empty
+        setAuth({});
+        try{
+            const response=await axios.get(LOGOUT_URL,{
+                headers:{
+                    'Content-Type' : 'application/json',
+                    'Accept' : 'application/json',
+                    'Authorization' : JSON.parse(localStorage.getItem('accessToken'))}
+            });
+        }catch(err){
+            console.error(err);
+        }
+    }
+    return logOut;
+}
+
+export default useLogOut; 
